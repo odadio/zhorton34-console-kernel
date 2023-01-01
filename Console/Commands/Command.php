b3bd528a0bfc9a0feecb96e9d941c7b8467efc6a
Afril <afril@youdingapp.com>
2023-01-01 11:16:43 +0700

    First commit

---
+++ Console/Commands/Command.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zhorton\Console\Commands;
+
+class Command extends \Boot\Foundation\Console\Command
+{
+    protected function arguments()
+    {
+        return [];
+    }
+}
---
+++ Console/Commands/DatabaseMigrateCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Zhorton\Console\Commands;
+
+
+class DatabaseMigrateCommand extends Command
+{
+    protected $name = 'db:migrate';
+    protected $help = 'migrate';
+    protected $description = 'Migration migrations to database';
+
+    public function handler()
+    {
+        shell_exec("./vendor/bin/phinx migrate");
+
+        $this->info("Successful when no error shown above");
+    }
+}
